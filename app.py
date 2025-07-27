136b27458d82e2d6a5d670c47856684267bedd29
Ali Alkhomayes <mr.alkhomayes.com>
2025-07-27 17:17:43 +0300

    sss

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
-import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -37,5 +36,4 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
